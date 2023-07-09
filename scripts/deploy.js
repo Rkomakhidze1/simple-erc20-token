e6182e563e1aedfe6fa3753f798c5dca6120d515
Rezo Komakhidze <rezokomakhidze@Rezos-MacBook-Pro.local>
2023-07-09 23:10:57 +0400

    added deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,32 +1,33 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
+const fs = require("fs/promises");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const Token = await hre.ethers.getContractFactory("Token");
+  const token = await Token.deploy("100");
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  const Exchange = await hre.ethers.getContractFactory("DecentralizedExchange");
+  const exchange = await Exchange.deploy(token, 100);
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  await token.waitForDeployment();
+  await exchange.waitForDeployment();
+  await writeDeploymentInfo(token, "token.json");
+  await writeDeploymentInfo(exchange, "exchange.json");
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
+async function writeDeploymentInfo(contract, filename = "") {
+  const data = {
+    network: hre.network.name,
+    contract: {
+      address: contract.target,
+      signerAddress: contract.runner.address,
+      abi: contract.interface.format(),
+    },
+  };
+
+  const content = JSON.stringify(data, null, 2);
+  await fs.writeFile(filename, content, { encoding: "utf-8" });
+}
+
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
